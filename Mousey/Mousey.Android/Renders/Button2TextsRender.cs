e85150ccb77bf757da7e5b081fdb55a66b1946b6
avraham calev <caleva@post.bgu.ac.il>
2019-03-05 00:53:37 +0200

    fixed the 2 text button

--- Mousey/Mousey.Android/Renders/Button2TextsRender.cs
+++ Mousey/Mousey.Android/Renders/Button2TextsRender.cs
@@ -8,6 +8,7 @@ using Mousey;
 using Mousey.Droid;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
+using Color = Xamarin.Forms.Color;
 
 [assembly: ExportRenderer(typeof(Button2Texts), typeof(Button2TextsRender))]
 namespace Mousey.Droid
@@ -48,31 +49,49 @@ namespace Mousey.Droid
             if (e.ActionMasked == MotionEventActions.Down)
             {
                 //button.SendMessagge(false);
-                Color newc = new Color(back.R - 0.1, back.G - 0.1, back.B - 0.1);
-                button.BackgroundColor = newc;
+                var newc = new Color(back.R - 0.1, back.G - 0.1, back.B - 0.1);
+                button.BackgroundColor = Color.Transparent;
+                Invalidate();
                 return true;
             }
             if (e.ActionMasked == MotionEventActions.Up)
             {
                 //button.SendMessagge(true);
                 button.BackgroundColor = back;
+                Invalidate();
                 return true;
             }
             return false;
         }
 
-        public override void Draw(Canvas canvas)
+        protected override void DispatchDraw(Canvas canvas)
         {
+            //base.DispatchDraw(canvas);
             if (button != null) {
                 var p = new Paint();
-                p.AntiAlias = true;
-                p.SetStyle(Paint.Style.Stroke);
-                p.StrokeWidth = 5;
-                p.Color = Android.Graphics.Color.Black;
+                Rect rect = new Rect();
+                p.StrokeWidth = 1;
 
-                canvas.DrawText(button.Text2,canvas.get)
-            base.Draw(canvas);
+                p.AntiAlias = true;
+                p.SetStyle(Paint.Style.Fill);
+                p.TextSize = 35;
+                System.Drawing.Color c = button.BackgroundColor;
+                p.SetARGB(255, c.R, c.G, c.B);
+                
+                canvas.GetClipBounds(rect);
+                canvas.DrawRoundRect(new RectF(rect), button.CornerRadius, button.CornerRadius, p);
+                p.Color = Android.Graphics.Color.Blue;
+                canvas.DrawText(button.Text2, rect.Left + 5, rect.Top + 40, p);
+                p.Color = Android.Graphics.Color.Black;
+                p.TextSize = 42;
+                canvas.DrawText(button.Text, rect.Left - 26 * button.Text.Length + rect.Width(), rect.Bottom - 10, p);
+
             }
+        }
+
+        protected override bool DrawChild(Canvas canvas, Android.Views.View child, long drawingTime)
+        {
+            return base.DrawChild(canvas, child, drawingTime);
         }
     }
 }
